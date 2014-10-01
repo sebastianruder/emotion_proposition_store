6b6031012786f8475b1b7c3eb59d130246312981
Sebastian Ruder <ruder.sebastian@gmail.com>
2014-10-01 12:24:51 +0100

    Updated emotionpatternextractor

--- src/EmotionPatternExtractor.java
+++ src/EmotionPatternExtractor.java
@@ -13,17 +13,17 @@ import java.util.regex.Pattern;
  */
 public class EmotionPatternExtractor {
 
-    List<Pattern> joyPatterns = new ArrayList<Pattern>();
-    List<Pattern> trustPatterns = new ArrayList<Pattern>();
-    List<Pattern> fearPatterns = new ArrayList<Pattern>();
-    List<Pattern> surprisePatterns = new ArrayList<Pattern>();
-    List<Pattern> sadnessPatterns = new ArrayList<Pattern>();
-    List<Pattern> disgustPatterns = new ArrayList<Pattern>();
-    List<Pattern> angerPatterns = new ArrayList<Pattern>();
-    List<Pattern> anticipationPatterns = new ArrayList<Pattern>();
+    Map<Pattern, String> joyPatterns = new ArrayMap<Pattern, String>();
+    Map<Pattern, String> trustPatterns = new ArrayMap<Pattern, String>();
+    Map<Pattern, String> fearPatterns = new ArrayMap<Pattern, String>();
+    Map<Pattern, String> surprisePatterns = new ArrayMap<Pattern, String>();
+    Map<Pattern, String> sadnessPatterns = new ArrayMap<Pattern, String>();
+    Map<Pattern, String> disgustPatterns = new ArrayMap<Pattern, String>();
+    Map<Pattern, String> angerPatterns = new ArrayMap<Pattern, String>();
+    Map<Pattern, String> anticipationPatterns = new ArrayMap<Pattern, String>();
     // TODO: look up how to initialize list
     List<List<Pattern>> patternList = new ArrayList<List<Pattern>>();
-    Map<String, List<Pattern>> emotionMap = new ArrayMap<String, List<Pattern>>();
+    Map<String, Map<Pattern, String>> emotionMap = new ArrayMap<String, Map<Pattern, String>>();
 
     /**
      * Initializes emotion map storing patterns pertaining to Plutchik's eight basic emotions
@@ -44,20 +44,20 @@ public class EmotionPatternExtractor {
      * storing these expressions as patterns in lists pertaining to the respective emotion.
      *
      * @param emotionTriggersFile: file path string of file containing emotion trigger expressions
-     * @return map (key: emotion word, value: list of patterns pertaining to this emotion)
+     * @return map (key: emotion word, value: map of emotion patterns and their respective right constituent)
      * @throws IOException
      */
-    public Map<String, List<Pattern>> extractEmotions(String emotionTriggersFile) throws IOException {
+    public Map<String, Map<Pattern, String>> extractEmotions(String emotionTriggersFile) throws IOException {
 
         InputStream inputStream = new FileInputStream(emotionTriggersFile);
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
 
         String line = reader.readLine();
-        while (line != null || line.equals("")) {
+        while (line != null) {
             String emotionWord = line.split("\t")[0];
             Pattern emotionPattern = Pattern.compile(line.split("\t")[1]);
-            emotionMap.get(emotionWord).add(emotionPattern);
-            System.out.println(line);
+            String rightConstituent = line.split("\t")[2];
+            emotionMap.get(emotionWord).put(emotionPattern, rightConstituent);
 
             // read next line
             line = reader.readLine();
@@ -70,10 +70,11 @@ public class EmotionPatternExtractor {
         // TODO: put this workflow in main of AgigaReader
         String filePath = "/home/sebastian/git/sentiment_analysis/emotion_trigger_patterns.txt";
         EmotionPatternExtractor emotionExtractor = new EmotionPatternExtractor();
-        Map<String, List<Pattern>> map = emotionExtractor.extractEmotions(filePath);
+        Map<String, Map<Pattern, String>> map = emotionExtractor.extractEmotions(filePath);
         Matcher m;
-        for (List<Pattern> patterns : map.values()) {
-            for (Pattern pattern : patterns) {
+        for (Map<Pattern, String> emotionMap: map.values()) {
+            for (Pattern pattern : emotionMap.keySet()) {
+                String rightConstituent = emotionMap.get(pattern);
                 m = pattern.matcher("sentence");
                 while (m.find()) {
                     // TODO: do something
