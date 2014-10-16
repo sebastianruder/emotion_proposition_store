1590e1bd2642df10fffdce252200f4644931925a
Sebastian Ruder <ruder.sebastian@gmail.com>
2014-10-16 18:35:49 +0100

    Added clarifying comment to Emotion class

--- src/Emotion.java
+++ src/Emotion.java
@@ -1,6 +1,11 @@
 /**
  * Created by sebastian on 29/09/14.
  */
+
+/**
+ * A class containing Plutchik's eight emotions that are used.
+ * Not needed yet; not sure if this will be useful.
+ */
 public enum Emotion {
     JOY, TRUST, FEAR, SURPRISE, SADNESS, DISGUST, ANGER, ANTICIPATION
 }
