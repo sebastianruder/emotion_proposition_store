5b83ad7f91db84a0acc43df488172835de6116a2
Sebastian Ruder <ruder.sebastian@gmail.com>
2015-03-29 12:02:28 +0100

    Put enums in old files, created enums for hard-coded strings

---
+++ src/Enums.java
@@ -0,0 +1,23 @@
+/**
+ * Created by sebastian on 28/03/15.
+ */
+public class Enums {
+
+    public enum Stats {
+        occurrences, matches
+    }
+
+    public enum Features {
+        isNP, passiveExists
+    }
+
+
+    /**
+     * Created by sebastian on 29/09/14.
+     * Enum containing Plutchik's eight emotions that are used.
+     * Not needed yet; not sure if this will be useful.
+     */
+    public enum Emotion {
+        JOY, TRUST, FEAR, SURPRISE, SADNESS, DISGUST, ANGER, ANTICIPATION
+    }
+}
