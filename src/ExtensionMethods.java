f7352a8298676d62fd337af29635ccbe9bf46bd9
Sebastian Ruder <ruder.sebastian@gmail.com>
2015-04-18 09:56:22 +0100

    Reversed list, refactored

--- src/ExtensionMethods.java
+++ src/ExtensionMethods.java
@@ -5,22 +5,26 @@ import java.util.*;
  */
 public class ExtensionMethods
 {
-    public static <K, V extends Comparable<? super V>> Map<K, V>
-    sortByValue( Map<K, V> map )
-    {
+    public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
         List<Map.Entry<K, V>> list =
                 new LinkedList<Map.Entry<K, V>>( map.entrySet() );
         Collections.sort(list, new Comparator<Map.Entry<K, V>>() {
             public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
-                return (o1.getValue()).compareTo(o2.getValue());
+                return - (o1.getValue()).compareTo(o2.getValue());
             }
         });
 
         Map<K, V> result = new LinkedHashMap<K, V>();
         for (Map.Entry<K, V> entry : list)
         {
-            result.put( entry.getKey(), entry.getValue() );
+            result.put(entry.getKey(), entry.getValue());
         }
         return result;
+    }
+
+    public static <T extends Comparable<? super T>> List<T> asSortedList(Collection<T> c) {
+        List<T> list = new ArrayList<T>(c);
+        java.util.Collections.sort(list);
+        return list;
     }
 }
